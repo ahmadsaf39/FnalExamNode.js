3f90eccb269ed09a6cd3b918642d9690af0fd403
Ahmad Safarjalani <ahmadsaf2007@gmail.com>
2026-05-23 13:12:22 +0300

    protect dashboard routes using protected route

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/layouts/Navbar";
 import Sidebar from "@/components/layouts/Sidebar";
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
 
 export default function DashboardLayout({
   children,
@@ -7,16 +8,18 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen">
-      <Navbar />
+    <ProtectedRoute>
+      <div className="min-h-screen">
+        <Navbar />
 
-      <div className="flex">
-        <Sidebar />
+        <div className="flex">
+          <Sidebar />
 
-        <main className="flex-1 p-6">
-          {children}
-        </main>
+          <main className="flex-1 p-6">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
