d59229d73a04d892abc7c401cf9370456c01643f
Ahmad Safarjalani <ahmadsaf2007@gmail.com>
2026-05-23 13:07:45 +0300

    create application constants utility

---
+++ utils/constant.ts
@@ -0,0 +1,18 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  OTP: "/otp",
+  DASHBOARD: "/dashboard",
+  STUDENTS: "/dashboard/students",
+  USERS: "/dashboard/users",
+};
+
+export const ROLES = {
+  ADMIN: "Admin",
+  USER: "User",
+};
+
+export const TOKEN_KEY = "token";
+
+export const APP_NAME =
+  "Student Management System";
